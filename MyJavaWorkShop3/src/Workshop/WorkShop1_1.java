f67b1302d3b9360705c7f1c628c4d10943111349
joe <tjdejr217@gmail.com>
2021-05-04 20:51:11 +0900

    good

--- MyJavaWorkShop3/src/Workshop/WorkShop1_1.java
+++ MyJavaWorkShop3/src/Workshop/WorkShop1_1.java
@@ -5,51 +5,52 @@ import java.util.*;
 
 public class WorkShop1_1 {
 	public static int getUserInput() {
-		 Scanner scanner = new Scanner(System.in);
-		 int inputString = scanner.nextInt();
-		 return inputString;
+		Scanner scanner = new Scanner(System.in);
+		String inputString = scanner.nextLine();
+		return Integer.parseInt(inputString);
 	}
-
-	public static void main(String[] args) {
-		int b1,b2,b3,b4; //메뉴 금액 변수를 저장
-		b1 = 2000;
-		b2 = 3000;
-		b3 = 1500;
-		b4 = 500;
-		
+	public static void main (String args[]) {
+		int b, b1, b2, b3;
+		b = 2000;
+		b1 = 3000;
+		b2 = 1500;
+		b3 = 500;
 		System.out.println("=========메뉴=========");
-		 System.out.println("1.아메리카노 2000원");
-		 System.out.println("2.카페라떼 3000원");
-		 System.out.println("3.베이글 1500원");
-		 System.out.println("4.크림치즈 500원"); 
-		 System.out.println("=========주문=========");
-		 System.out.println("1.아메리카노 주문 수량:");
-		 int a1 = getUserInput(); // 
-		 System.out.println("2.카페라떼 주문수량 :");
-		 int a2 = getUserInput();
-		 System.out.println("3.베이글 주문수량 :");
-		 int a3 = getUserInput();
-		 System.out.println("4.크림치즈 주문수량 :");
-		 int a4 = getUserInput();
-		 System.out.println("=========금액=========");
-		 System.out.printf("아메리카노: %d원%n", a1*b1);
-		 System.out.printf("카페라떼: %d원%n", a2*b2);
-		 System.out.printf("베이글: %d원%n", a3*b3 );
-		 System.out.printf("크림치즈: %d원%n", a4*b4);
+		System.out.println("1.아메리카노 : 2000원");
+		System.out.println("2.카페라떼 : 3000원");
+		System.out.println("3.베이글 : 1500원");
+		System.out.println("4.크림치즈 : 500원");
+		System.out.println("=========주문=========");
 		 
-		 System.out.println("=========주문=========");
-		 int totalPrice = (a1 * b1) + (a2 * b2) + (a3 *b3) + (a4 * b4);
-		 System.out.println("총 구매 금액:" + totalPrice+"원");
-		 float point = 0;
-		 if(totalPrice>= 30000) {
-			 point = totalPrice*0.02f;
-		 }else if(12000 <=totalPrice && 30000 >totalPrice) {
-			 point = totalPrice*0.01f;
-		 }else {
-			 System.out.println("포인트 적립은 12000원 이상부터 입니다 더 사세요.^^");
-		 }
-		 System.out.println("포인트적립:"+(int)point+"원");	
-	
+		System.out.println("아메리카노 주문 수량 : ");
+		int a = getUserInput();
+		System.out.println("카페라떼 주문 수량 : ");
+		int a1 = getUserInput();
+		System.out.println("베이글 주문 수량 : ");
+		int a2 = getUserInput();
+		System.out.println("크림치즈 주문 수량 : ");
+		int a3 = getUserInput();
+		
+		System.out.println("=========금액=========");
+		System.out.printf("아메리카노: %d원 %n",a*b);
+		System.out.printf("카페라떼: %d원 %n",a1*b1);
+		System.out.printf("베이글: %d원 %n",a2*b2);
+		System.out.printf("크림치즈: %d원 %n",a3*b3);
+		
+		System.out.println("========주문확인=========");
+		int total = (a * b) + (a1 * b1) + (a2 * b2) + (a3 * b3);
+		System.out.println("총 구매 금액:" +total+"원");
+		
+		float point = 0;
+		if(total>=30000) {
+			point = total*0.02f;
+		}else if(12000 <=total && 30000> total) {// 논리 연산자 잘 체크!!
+			point = total*0.01f;
+		}else {
+			System.out.println("포인트 적립을 위해 더 힘써주세요.^^");
+		}
+		System.out.println("포인트 적립 :" +(int)point+"원");
+		
+		
 	}
-
 }
--- MyJavaWorkShop3/src/Workshop/WorkShop2.java
+++ MyJavaWorkShop3/src/Workshop/WorkShop2.java
@@ -1,10 +1,64 @@
 package Workshop;
-
+import java.util.*;
 public class WorkShop2 {
-
+	public static double getUserInput() {
+		Scanner scanner = new Scanner(System.in);
+		Double inputString = scanner.nextDouble();
+		return inputString;
+	}
 	public static void main(String[] args) {
-		
 
+	double a,a1,a2,a3,a4;
+	System.out.println("Computer Science 성적을 입력하세요 :");
+		a = getUserInput();
+	System.out.println("JavaPrograming 성적을 입력하세요 :");
+		a1 = getUserInput();
+	System.out.println("공학수학 성적을 입력하세요 :");
+		a2 = getUserInput();
+	System.out.println("오페라의 이해  성적을 입력하세요: ");
+		a3 = getUserInput();
+	System.out.println("배드민턴 성적을 성적을 입력하세요 :");
+		a4 = getUserInput();
+	System.out.println("==========================");
+	double avg = (a+a1+a2+a3+a4) /5;
+	System.out.println("평점은"+avg+"점 입니다.");
+	// 각 과목별 성적이 2.5이상(한 과목이라도 2.5 미만시 ,과락으로 장학금 탈락
+	//좀더 간결한 코드가 없을런지...?
+	// 삼항 연산자 조건....?
+	// 조건1) 평균이 3.7 이상 2)각 과목별 성적이 2.5이상 (2.5미만이라면 장학금 탈락)
+	if(avg <3.7) {
+		System.out.println("평균이 안됩니다.");
+	}else if(a<2.5 || a1<2.5 || a2<2.5 || a3<2.5 || a4<2.5) {
+		System.out.println("과락이야 친구야.");
+	}
+	else {
+		System.out.println("다음 학기 장학금 대상자 입니다.");
+	}
+	
+	
+	double b,b1,b2,b3,b4;
+	System.out.println("Computer Science 성적을 입력하세요 :");
+		b = getUserInput();
+	System.out.println("JavaPrograming 성적을 입력하세요 :");
+		b1 = getUserInput();
+	System.out.println("공학수학 성적을 입력하세요 :");
+		b2 = getUserInput();
+	System.out.println("오페라의 이해  성적을 입력하세요: ");
+		b3 = getUserInput();
+	System.out.println("배드민턴 성적을 성적을 입력하세요 :");
+		b4 = getUserInput();
+	System.out.println("==========================");
+	double avg1 = (b+b1+b2+b3+b4) /5;
+	System.out.println("평점은"+avg1+"점 입니다.");
+	if(avg <3.7) {
+		System.out.println("평균이 안됩니다.");
+	}else if(b<2.5 || b1<2.5 || b2<2.5 || b3<2.5 || b4<2.5) {
+		System.out.println("과락이야 친구야.");
+	}
+	else {
+		System.out.println("다음 학기 장학금 대상자 입니다.");
+	}
+	
 	}
 
 }
