d3d06cecf20dad96d57294c84767b1acb93d9637
seongdeokjo <tjdejr217@gmail.com>
2021-05-24 16:14:27 +0900

    calc commit

---
+++ FirstJava/src/test/Calculator.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Calculator {
+	void add(int a, int b) {
+		System.out.println(a + b);
+	}
+	void sub(int a, int b) {
+		System.out.println(a - b);
+	}
+	void multi(int a, int b) {
+		System.out.println(a * b);
+	}
+	void divide(int a, int b) {
+		System.out.println(a / b);
+	}
+}
---
+++ FirstJava/src/test/Member.java
@@ -0,0 +1,12 @@
+package test;
+public class Member {
+
+	public static void main(String[] args) {
+		System.out.println("조성덕");
+		//인스턴스 생성 
+		Calculator calc = new Calculator();
+		calc.add(20, 30);
+		
+	}
+
+}
