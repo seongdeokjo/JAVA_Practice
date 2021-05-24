d6fdb140b5f5c69d7447f2af88d6a1453fd9a742
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-05-24 13:13:23 +0900

    Delete HelloJava.java

--- HelloJava.java
+++
@@ -1,8 +0,0 @@
-// HelloJava.java
-// javac.exe => javac HelloJava.java
-// java.exe => java HelloJava
-class HelloJava {
-	public static void main(String args[]) {
-		System.out.println("Hello, Java");	
-	}
-}
