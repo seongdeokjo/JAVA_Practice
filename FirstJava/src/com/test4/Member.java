5c40795a07c64d060a18f3bf2fe2c76d1659a325
seongdeokjo <tjdejr217@gmail.com>
2021-05-26 11:18:18 +0900

    2교시 수업내용. 수정코드
    this() ,접근제어자, final

--- FirstJava/src/com/test4/Member.java
+++ FirstJava/src/com/test4/Member.java
@@ -1,10 +1,11 @@
 package com.test4;
 
 public class Member {
-//	 순서 -> 변수 - 생성자 - 메서드 순 
-	String name;
-	String ph;
-	String major;
+	// 순서 -> 변수 - 생성자 - 메서드 순
+	private String name;
+
+	final String ph;
+	 String major;
 	int grade;
 	String email;
 	String birth;
@@ -13,11 +14,17 @@ public class Member {
 	// 생성자 -> 초기화 메서드, 인스턴스를 생성할 때 단 한번, 인스턴스 변수를 초기화 한다.
 	// 오버로딩 -> 이름이 같은 메소드 정의
 	public Member(String name, String ph, String major, int grade, String email) {
-//		자동으로 해당 파라미터에 적합한 자신의 생성자를 호출한다.
-		this(name,ph,major,grade,email,"2000","부산");
+		// 자동으로 해당 파라미터에 적합한 자신의 생성자를 호출한다.
+		this(name, ph, major, grade, email, null, null);
+		// this.name = name;
+		// this.ph = ph;
+		// this.major = major;
+		// this.grade = grade;
+		// this.email = email;
 	}
 
 	public Member(String name, String ph, String major, int grade, String email, String birth, String address) {
+		// this(name,ph,major,grade,email);
 		this.name = name;
 		this.ph = ph;
 		this.major = major;
@@ -26,14 +33,41 @@ public class Member {
 		this.birth = birth;
 		this.address = address;
 	}
+	
+	public Member(String name, String ph, String major) {
+		this(name, ph, major, 0, null, null, null);
+	}
+	 Member() {
+		this.ph = "010-9999-9999";
+	}
 
+	
 	public void info() {
 		System.out.println("이름: " + this.name);
 		System.out.println("전화번호: " + this.ph);
 		System.out.println("전공: " + this.major);
 		System.out.println("학년: " + this.grade);
 		System.out.println("이메일주소: " + this.email);
-		System.out.println("생년월일: " + this.birth);
-		System.out.println("주소: " + address);
+
+		if (this.birth == null) {
+			System.out.println("생일: 입력된 데이터가 없습니다.");
+		} else {
+			System.out.println("생년월일: " + this.birth);
+		}
+		if (this.address != null) {
+			System.out.println("주소: " + address);
+		} else {
+			System.out.println("주소: 입력된 데이터가 없습니다.");
+		}
+
 	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 }
--- FirstJava/src/com/test4/MemberTest.java
+++ FirstJava/src/com/test4/MemberTest.java
@@ -6,8 +6,13 @@ public class MemberTest {
 		Member member = new Member("홍길동", "010-123-1234", "전기", 3, "111@111.com");
 		Member member1 = new Member("이순신", "010-321-4321", "전자", 1, "222@222.com", "2017", "서울");
 		
+		System.out.println("수정 전 이름 : "+member.getName());
+		member.setName("찰스");
+		System.out.println("수정 후 이름 : "+member.getName());
 		member.info();
-		System.out.println("==============");
+//		member.ph = "000000"; //final로 값이 정해졌기 때문에 바꿀 수 없다.
+		
+		System.out.println("=======================");
 		member1.info();
 	}
 }
