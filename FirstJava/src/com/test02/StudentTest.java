457e9e656920fd094b7fc1df9288417efa129e4d
seongdeokjo <tjdejr217@gmail.com>
2021-05-26 12:42:51 +0900

    Student 배열 수정 ver

--- FirstJava/src/com/test02/StudentTest.java
+++ FirstJava/src/com/test02/StudentTest.java
@@ -1,6 +1,11 @@
 package com.test02;
 public class StudentTest {
 	public static void main(String[] args) {
+		int mathSum = 0;
+		int engSum = 0;
+		int korSum = 0;
+		double sumClass = 0;
+		double avgClass = 0;
 		//Student 객체배열을 만들고 인스턴스와 생성자 초기화 
 		Student[] stu = {
 			new Student("김씨",95,90,80),
@@ -14,19 +19,26 @@ public class StudentTest {
 			new Student("하씨",95,90,70),
 			new Student("구씨",75,75,80)
 		};
-		double sum1 = 0;
-		double avg1 = 0;
+	
 		// 개인 총점과, 개인평균을 구하기 위한 반복문 작성 
 		for(int i =0; i <stu.length; i++) {		
 			System.out.println("["+(i+1)+"]"+stu[i]+"-> 개인 총점: "+stu[i].total()+
 								", 개인 평균:"+Math.round(stu[i].avg()*100)/100.0);
-		//sum1과 출력문의 순서가 바뀌게 된다면 avg() 메서드안에 개인 총점 점수가 저장되어있지않기 때문에 avg1값은 0이 출력된다.
-			
-			sum1 += stu[i].avg();
+		//sum1과 출력문의 순서가 바뀌게 된다면 avg() 메서드안에 개인 총점 점수가 저장되어있지않기 때문에 avg1값은 0이 출력된다.			
+			sumClass += stu[i].avg();
+		// 각 과목의 평균값을 구하기 위해 각각의 get메소드를 이용하여 필요한 값만 호출한다. 	
+			mathSum += stu[i].getMath();
+			engSum += stu[i].getEng();
+			korSum += stu[i].getKor();
 		}
-		//반평균을 구하기 위해  저장된 개인 평균 값에 배열의 수 만큼 나눠주면 반 평균 점수가 된다./
-			
-		avg1 = sum1 / stu.length;
-		System.out.println("반 평균: "+Math.round(avg1*100)/100.0);						
+		//반평균을 구하기 위해  저장된 개인 평균 값에 배열의 수 만큼 나눠주면 반 평균 점수가 된다./			
+		avgClass = sumClass / stu.length;
+		System.out.println("반 평균: "+Math.round(avgClass*100)/100.0);
+		System.out.println();
+		System.out.println("==============각 과목의 평균==============");
+		System.out.println("수학 과목의 평균은 =>"+(double)mathSum/stu.length);
+		System.out.println("영어과목의 평균은 =>"+(double)engSum/stu.length);
+		System.out.println("국어 과목의 평균은 =>"+(double)korSum/stu.length);
+		
 	}
 }
