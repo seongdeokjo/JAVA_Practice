be3fced7c30fed99dccfdd5a520bbd87551dd1f2
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-05-27 16:19:58 +0900

    상속관계

--- FirstJava/src/com/test7/ExtendsSample01.java
+++ FirstJava/src/com/test7/ExtendsSample01.java
@@ -2,18 +2,30 @@ package com.test7;
 
 class Car{
 	int gasolinegauge;
+	Car(int gas){
+		this.gasolinegauge = gas;
+	}
 }
 class HybridCar extends Car {
 	int electronicGauge;
-	HybridCar(int gasolinegauge, int electronicGauge) {
-		super();
-		this.electronicGauge = electronicGauge;
+	HybridCar(int gas, int eg) {
+		super(gas);
+		electronicGauge = eg;
+	}
+	HybridCar(int gas) {
+		super(gas);
 	}
 }
 class HybridWaterCar extends HybridCar {
 	int waterGauge;
-	HybridWaterCar(int gasolinegauge, int electronicGauge,int waterGauge) {
-		super(gasolinegauge, electronicGauge);
+	HybridWaterCar(int gas) {
+		super(gas);
+	}
+	HybridWaterCar(int gas, int eg) {
+		super(gas, eg);
+	}
+	HybridWaterCar(int gas, int eg,int waterGauge) {
+		super(gas, eg);
 		this.waterGauge = waterGauge;
 	}
 	
@@ -23,15 +35,10 @@ class HybridWaterCar extends HybridCar {
 		System.out.println("잔여 가솔린 : "+ waterGauge);
 	}
 }
-
-
-
 public class ExtendsSample01 {
 	
-
 	public static void main(String[] args) {
 		HybridWaterCar car = new HybridWaterCar(50,30,40);
 		car.showCrrentGauge();
 	}
-
 }
