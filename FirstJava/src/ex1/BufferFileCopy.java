d0ac18492b426f19dac79b9dbed43dd918eaf1ec
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-06-02 12:20:04 +0900

    day21 i/o buffer

--- FirstJava/src/ex1/BufferFileCopy.java
+++ FirstJava/src/ex1/BufferFileCopy.java
@@ -2,7 +2,7 @@ package ex1;
 
 import java.io.*;
 
-public class ByteFileCopy {
+public class BufferFileCopy {
 
 	public static void main(String[] args)  {
 		
@@ -10,23 +10,25 @@ public class ByteFileCopy {
 			//1. 파일을 바이너리 데이를 읽어올 스트림 생성 : FileInputStream
 			InputStream in = new FileInputStream("origin.pdf");
 			//2.파일을 쓰기위한 스트림 생성 : FileOutputStream 
-			OutputStream out = new FileOutputStream("copy.pdf");
+			OutputStream out = new FileOutputStream("copy2.pdf");
 			
 			//copy한 데이터의 크기
 			int copyByte = 0;
-			//파일에서 읽어올 바이트 데이터
-			int bData = -1;
+			//파일에서 읽어올 바이트 데이터 배열
+			byte[] buf = new byte[1024]; //1kb 사이즈의 배열 
+			//읽어온 데이터 배열의 개수
+			int readLen = 0;
 			
 			while(true) {
-				bData = in.read();
+				readLen = in.read(buf); // 완료시점 -> -1 반환
 				
 				//탈출의 조건 : 파일의 모든 데이터를 읽은 경우 
-				if(bData == -1) {
+				if(readLen == -1) {
 					break;
 				}
 				//출력 : 파일에 데이터를 쓴다.
-				out.write(bData);
-				copyByte++;
+				out.write(buf,0,readLen);
+				copyByte += readLen;
 			}
 			// 스트림 닫기 
 			in.close();
