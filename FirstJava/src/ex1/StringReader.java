dd860555fce1b3a7ef0c508d31a0164096b4989e
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-06-02 15:25:20 +0900

    day21 5교시

---
+++ FirstJava/src/ex1/StringReader.java
@@ -0,0 +1,30 @@
+package ex1;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class StringReader {
+
+	public static void main(String[] args) {
+		try {
+			
+			BufferedReader in = new BufferedReader(new FileReader("String.txt"));
+			String str;
+			while(true) {
+				str = in.readLine();
+				if(str == null) {
+					break;
+				}
+				System.out.println(str);
+			}
+			in.close();
+			System.out.println();	
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
