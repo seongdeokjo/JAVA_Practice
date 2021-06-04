6409f85e49aed03aaa0cf2b48591aa58b4b4cf76
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-06-04 16:25:20 +0900

    day23 6교시

--- MyJavaNewPractice/src/ex/th2/MessageServer.java
+++ MyJavaNewPractice/src/ex/th2/MessageServer.java
@@ -8,53 +8,78 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class MessageServer {
-
-	
-	
 	
 	public static void main(String[] args) {
 		DataInputStream din = null;
 		DataOutputStream dout = null;
-		ServerSocket ss = null;
+		ServerSocket ss = null;	
 		Socket socket = null;
-				
 		try {
-			 ss = new ServerSocket(8888);
-		
-			 socket = ss.accept();
+			ss = new ServerSocket(8888);
 			
-		din = new DataInputStream(socket.getInputStream());
-		 dout = new DataOutputStream(socket.getOutputStream());
+			System.out.println("서버 실행");
+			
+			System.out.println("=================================");
+		
+			socket = ss.accept();
+			
+			System.out.println("client 접속");
+			System.out.println("=================================");
+			
+			din = new DataInputStream(socket.getInputStream());
+			dout = new DataOutputStream(socket.getOutputStream());
 			
 			Scanner scan = new Scanner(System.in);
 					//초기화시 공백문자열도 인스턴스가 생기므로 null사용
 			String str1  = null; // 받는 메세지
-			String str2 = null; // 보내는 메세지 
+			String str2 = ""; // 보내는 메세지 
 			
 			while(!str2.equals("exit")) {
 				
 				str1 = din.readUTF();
-				System.out.println(str1);
+				System.out.println("Client Message : "+str1);
 				
 				str2 = scan.nextLine();
 				dout.writeUTF(str2);
 				dout.flush();
-			}
-			
-			
-		
-			
-			
-			
-			
-		
+			}		
 		} catch (IOException e) {
 			e.printStackTrace();
 		}finally {
-			din.close();
-			dout.close();
-			socket.close();
-			ss.close();
+			
+			Util.close(din);
+			Util.close(dout);
+			Util.close(ss);
+			Util.close(socket);
 		}
+			/*if(din != null) {
+				try {
+				din.close();
+				} catch (IOException e) {
+				e.printStackTrace();
+				}
+			}
+			if(dout != null) {
+				try {
+					dout.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(socket != null) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if(ss != null) {
+				try {
+					ss.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}*/		
 	}
 }
