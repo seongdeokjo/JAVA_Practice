c6fc85569ce18ae5696d5e2960ac1d13dca03778
seongdeokjo <tjdejr217@gmail.com>
2021-06-07 09:30:01 +0900

    commit

--- MyJavaNewPractice/src/ex/io/IOEx5.java
+++ MyJavaNewPractice/src/ex/io/IOEx5.java
@@ -8,29 +8,31 @@ import java.io.StringWriter;
 public class IOEx5 {
 
 	public static void main(String[] args) {
-		 InputThread inThread = new InputThread("InputTread");
-		 OutputThread outThread = new OutputThread("OutputThread");
-		 
-		 inThread.connect(outThread.getOutput());
-		 
-		 inThread.start();
-		 outThread.start();
-		 
+		InputThread inThread = new InputThread("InputTread");
+		OutputThread outThread = new OutputThread("OutputThread");
+
+		inThread.connect(outThread.getOutput());
+
+		inThread.start();
+		outThread.start();
+
 	}
 
 }
+
 class InputThread extends Thread {
 	PipedReader input = new PipedReader();
 	StringWriter sw = new StringWriter();
-	
+
 	InputThread(String name) {
 		super(name);
 	}
+	//Thread 오버라이딩 
 	public void run() {
 		int data = 0;
-		
+
 		try {
-			while((data = input.read()) != -1) {
+			while ((data = input.read()) != -1) {
 				sw.write(data);
 			}
 			System.out.println(sw.toString());
@@ -38,49 +40,50 @@ class InputThread extends Thread {
 			e.printStackTrace();
 		}
 	}
+
 	public PipedReader getInput() {
 		return input;
 	}
-	
+
 	public void connect(PipedWriter output) {
 		try {
 			input.connect(output);
-		}catch(IOException e) {
-			
+		} catch (IOException e) {
+
 		}
 	}
-	
 }
 
-
 class OutputThread extends Thread {
-	PipedWriter output = new PipedWriter(); 
-	
+	PipedWriter output = new PipedWriter();
+
 	OutputThread(String name) {
 		super(name);
 	}
-	
+	//Thread 오버라이딩 
 	public void run() {
 		try {
 			String msg = "Hello";
-			System.out.println(getName()+ msg);
+			System.out.println(getName() + msg);
 			output.write(msg);
 			output.close();
-			
-		}catch(IOException e) {
-			
+
+		} catch (IOException e) {
+
 		}
-		
+
 	}
+
 	public PipedWriter getOutput() {
 		return output;
 	}
+
 	public void connect(PipedReader input) {
 		try {
 			output.connect(input);
-			
-		}catch(IOException e) {
-			
+
+		} catch (IOException e) {
+
 		}
 	}
 }
