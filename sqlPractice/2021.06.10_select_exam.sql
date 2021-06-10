8b1fc0ca4bdde37c7e452eb0e4bd3f0be2c61474
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-06-10 16:02:46 +0900

    수정 ver2

--- sqlPractice/2021.06.10_select_exam.sql
+++ sqlPractice/2021.06.10_select_exam.sql
@@ -64,7 +64,7 @@ from emp
 where mgr is null
 ;
 ​
-11. 커미션을 받을 수 있는 자격이 되는 사원의 이름, 급여, 커미션을 출력하되 급여 및 커미션을 기준으로 내림차순 정렬하여 표시하시오.
+--11. 커미션을 받을 수 있는 자격이 되는 사원의 이름, 급여, 커미션을 출력하되 급여 및 커미션을 기준으로 내림차순 정렬하여 표시하시오.
 select ename, sal, comm
 from emp
 where comm is not null
@@ -82,12 +82,12 @@ select ename
 from emp
 where ename like '%A%' and ename like '%E%'
 ;
-​
+​                                   --and
 --14. 담당업무가 CLERK, 또는 SALESMAN이면서 급여가 $1600, $950 또는 $1300이 아닌 사원의 이름, 담당업무, 급여를 출력하시오.
-select ename, job, sal
+select ename , job, sal
 from emp
-where ( job = 'CLERK' or  job = 'SALESMAN') and (sal not in(1600,950,1300));
-
+where (job = 'CLERK' or job = 'SALESMAN') and not sal in (1600,950,1300)
+;
 
 --15. 커미션이 $500 이상인 사원의 이름과 급여 및 커미션을 출력하시오.
 select ename, sal, comm
