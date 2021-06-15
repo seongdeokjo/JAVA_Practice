46a436ae1be409690ecc2f85e18af0e6e62c8aa7
seongdeokjo <tjdejr217@gmail.com>
2021-06-15 19:47:15 +0900

    instance and class exam

--- JAVA/FirstJava/src/com/test4/MemberTest_1.java
+++ JAVA/FirstJava/src/com/test4/MemberTest_1.java
@@ -1,11 +1,11 @@
 package com.test4;
 
-public class Member {
+class Member {
 	// 순서 -> 변수 - 생성자 - 메서드 순
 	private String name;
 
 	final String ph;
-	 String major;
+	String major;
 	int grade;
 	String email;
 	String birth;
@@ -13,6 +13,7 @@ public class Member {
 
 	// 생성자 -> 초기화 메서드, 인스턴스를 생성할 때 단 한번, 인스턴스 변수를 초기화 한다.
 	// 오버로딩 -> 이름이 같은 메소드 정의
+	// 생일과 주소는 저장하지 않을 수 있는 생성자
 	public Member(String name, String ph, String major, int grade, String email) {
 		// 자동으로 해당 파라미터에 적합한 자신의 생성자를 호출한다.
 		this(name, ph, major, grade, email, null, null);
@@ -23,6 +24,7 @@ public class Member {
 		// this.email = email;
 	}
 
+	// 모든 정보를 저장하도록 하는 생성자
 	public Member(String name, String ph, String major, int grade, String email, String birth, String address) {
 		// this(name,ph,major,grade,email);
 		this.name = name;
@@ -33,16 +35,17 @@ public class Member {
 		this.birth = birth;
 		this.address = address;
 	}
+
 	
 	public Member(String name, String ph, String major) {
 		this(name, ph, major, 0, null, null, null);
 	}
-	//final ph 생성자를 통한 초기화 
-	 Member() {
+
+	// final ph 생성자를 통한 초기화
+	Member() {
 		this.ph = "010-9999-9999";
 	}
 
-	
 	public void info() {
 		System.out.println("이름: " + this.name);
 		System.out.println("전화번호: " + this.ph);
@@ -62,13 +65,24 @@ public class Member {
 		}
 
 	}
-	
+
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+	}
+}
+
+public class MemberTest_1 {
+	public static void main(String ages[]) {
+		Member member1 = new Member("홍길동","010-1111-1111","무술",4,"gil@naver.com","901111","seoul");
+		Member member2 = new Member("길삼","010-2222-2222","문학",3,"dong@gmail.com");
+		member1.info();
+		System.out.println("=====================================");
+		member2.info();
+		
 	}
 
 }
