675555a42145ceefa902c43d60991e68c6e29b8c
seongdeokjo <tjdejr217@gmail.com>
2021-06-17 11:18:47 +0900

    day32 index

---
+++ sqlPractice/2021.06.17 index.sql
@@ -0,0 +1,22 @@
+--2021 06.17
+--index: 검색을 빠르게 하기위한 객체
+--물리적인 저장 공간이 필요
+--생성 시간이 필요
+--많은 index는 dml 작업이 많은 경우 성능 저하가 온다. 
+
+--emp01 테이블을 복사 -> 게속 복사
+drop table emp01;
+
+create table emp01
+as
+select * from emp
+;
+
+insert into emp01 select * from emp01;
+
+insert into emp01 (empno,ename) values (7777,'KING');
+insert into emp01 (empno,ename) values (7777,'SON');
+select * from emp01 where ename ='KING';
+select * from emp01 where ename = 'SON';
+
+--emp01 테이블에 ename 컬럼을 index 설정 
