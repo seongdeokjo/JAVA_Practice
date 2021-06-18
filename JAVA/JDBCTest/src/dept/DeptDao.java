a1e50f1eb924ad28c5923fa1838a2d2fc70eb9a9
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-06-18 16:20:21 +0900

    day33 jdbc dept dao

--- JAVA/JDBCTest/src/dept/DeptDao.java
+++ JAVA/JDBCTest/src/dept/DeptDao.java
@@ -1,6 +1,7 @@
 package dept;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -23,6 +24,8 @@ public class DeptDao {
 			//결과 받아오기
 			rs = stmt.executeQuery(sql);
 			
+			list = new ArrayList<>();
+			
 			//데이터를 Dept 객체로 생성 -> list에 저장
 			while(rs.next()) {
 				list.add(new Dept(rs.getInt(1),rs.getString(2),rs.getString(3)));
@@ -30,9 +33,17 @@ public class DeptDao {
 			
 			
 			
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
 			if(stmt != null) {
 				try {
 					stmt.close();
@@ -45,4 +56,69 @@ public class DeptDao {
 		
 		return list;		
 	}
+
+	// 2.Dept 테이블에 데이터 저장하는 메소드
+	// 반영 횟수 반환 
+	// 사용자로 부터 데이터 받기 -> Dept 객체
+	int insertDept(Connection conn, Dept dept) {
+		int result = 0;
+		
+		//전달받은 Dept 객체의 데이터로 Dept 테이블에 저장 -> 결과 값을 반환 
+		PreparedStatement pstmt = null;
+		
+		try {
+			String sql = "insert into dept values (DEPT01_DEPTNO_SEQ.nextval, ?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dept.getDname());
+			pstmt.setString(2, dept.getLoc());
+			
+			
+			result =pstmt.executeUpdate();
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+	
+	//3. Dept 테이블의 데이터 수정 메소드
+	// 반영된 행의 개수 반환
+	// 사용자로부터 데이터를 받아서 처리 -> Dept 객체
+	int editDept(Connection conn, Dept dept) {
+	int result = 0;
+		
+		//전달받은 Dept 객체의 데이터로 Dept 테이블에 저장 -> 결과 값을 반환 
+		PreparedStatement pstmt = null;
+		
+		try {
+			String sql = "update dept set dname =?, loc =? where deptno =? ";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, dept.getDname());
+			pstmt.setString(2, dept.getLoc());
+			pstmt.setInt(3, dept.getDeptno());
+			
+			result =pstmt.executeUpdate();
+		
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+	
 }
--- JAVA/JDBCTest/src/dept/DeptMain.java
+++ JAVA/JDBCTest/src/dept/DeptMain.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 public class DeptMain {
 
@@ -23,15 +24,52 @@ public class DeptMain {
 			
 			conn = DriverManager.getConnection(jdbcUrl, user, pw);
 			System.out.println("데이터베이스 연결 성공!!");
-			
-			
+				
 			DeptDao dao = new DeptDao();
 			
 			List<Dept> list = dao.getDeptList(conn);
 			
-			for(Dept dept : list) {
+		/*	for(Dept dept : list) {
 				System.out.println(dept);
-			}
+			} 
+		*/
+		Scanner scan = new Scanner(System.in);
+		
+	/*	System.out.println("부서입력을 시작합니다.");
+		System.out.println("부서번호 부서이름 부서위치 순으로 입력해주세요.");
+		System.out.println("예) 50 dev seoul");
+		String deptData = scan.nextLine();
+		String[] deptDatas = deptData.split(" ");
+		
+		//Dept 객체 생성 -> Dao 메소드 호출 
+		Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);	
+		
+		int result = dao.insertDept(conn, dept);
+		if(result > 0) {
+			System.out.println("입력 성공!");
+		}else {
+			System.out.println("입력 실패");
+		}*/
+		
+		
+		System.out.println("부서수정을 시작합니다.");
+		System.out.println("수정하고자하는 부서번호 새부서이름  새부서위치 순으로 입력해주세요.");
+		System.out.println("예) 50 dev seoul");
+		String deptData = scan.nextLine();
+		String[] deptDatas = deptData.split(" ");
+		
+		//Dept 객체 생성 -> Dao 메소드 호출 
+		Dept dept = new Dept(Integer.parseInt(deptDatas[0]), deptDatas[1], deptDatas[2]);	
+		
+		int result = dao.editDept(conn, dept);
+		if(result > 0) {
+			System.out.println("수정 성공!");
+		}else {
+			System.out.println("수정 실패");
+		}
+		
+		
+		
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
