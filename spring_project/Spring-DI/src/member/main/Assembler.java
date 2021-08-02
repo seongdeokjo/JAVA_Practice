60eb54ec338a32a7e6fd46fb46417624a47c93ce
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-08-02 16:24:54 +0900

    assembler

---
+++ spring_project/Spring-DI/src/member/main/Assembler.java
@@ -0,0 +1,42 @@
+package member.main;
+
+import javax.imageio.spi.RegisterableService;
+
+import member.dao.MemberDao;
+import member.service.ChangePasswordService;
+import member.service.MemberRegService;
+
+public class Assembler {
+	
+	// 컨테이너 : 인스턴스를 관리 -> 생성, 보관, 실행, 필요한 객체를 반환, 소멸
+	// 조립기 클래스의 역활
+	// 각각의 인스턴스를 생성
+	// 의존 주입을 해준다.
+	// 필요한 객체를 반환 해주는 기능
+	
+	private MemberDao dao;
+	private MemberRegService regService;
+	private ChangePasswordService passwordService;
+	
+	public Assembler() {
+		//인스턴스를 생성 -> 의존 주입
+		dao = new MemberDao();
+		regService = new MemberRegService(dao);
+		passwordService = new ChangePasswordService(dao);
+	}
+
+	public MemberDao getDao() {
+		return dao;
+	}
+
+	public MemberRegService getRegService() {
+		return regService;
+	}
+
+	public ChangePasswordService getPasswordService() {
+		return passwordService;
+	}
+	
+	
+	
+}
