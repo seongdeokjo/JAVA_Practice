f15cdc2f3b7f32f59db4d722941a5b9fb2f5f7a8
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-08-02 15:42:06 +0900

    member

---
+++ spring_project/Spring-DI/src/member/main/MainForMemberManager.java
@@ -0,0 +1,18 @@
+package member.main;
+
+public class MainForMemberManager {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	static void printMenu() {
+		System.out.println();
+		System.out.println("명령어 사용법");
+		
+	}
+	
+	
+}
---
+++ spring_project/Spring-DI/src/member/service/ChangePasswordService.java
@@ -0,0 +1,30 @@
+package member.service;
+
+import member.dao.Dao;
+import member.domain.Member;
+
+public class ChangePasswordService {
+	Dao dao;
+	
+	public ChangePasswordService(Dao dao) {
+		this.dao = dao;
+	}
+	
+	public void changePassword(String email,String oldPw, String newPw) throws Exception {
+		Member member = dao.selectByEmail(email);
+		
+		if(member == null) {
+			throw new Exception("존재하지 않는 회원!");
+		}
+		
+		member.changePassword(oldPw, newPw);
+		
+		dao.update(member);
+		
+		
+	}
+	
+	
+	
+	
+}
