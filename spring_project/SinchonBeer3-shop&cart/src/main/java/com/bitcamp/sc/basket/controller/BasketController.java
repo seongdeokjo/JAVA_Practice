2789ad79c6476b9f62a838beae8a7eb0dbb216dc
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-09-16 00:05:13 +0900

    basket 부분 modal login 처리 중

--- spring_project/SinchonBeer3-shop&cart/src/main/java/com/bitcamp/sc/basket/controller/BasketController.java
+++ spring_project/SinchonBeer3-shop&cart/src/main/java/com/bitcamp/sc/basket/controller/BasketController.java
@@ -2,12 +2,13 @@ package com.bitcamp.sc.basket.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/basket")
 public class BasketController {
 	
-	@RequestMapping("/basket")
+	@RequestMapping(value = "/basket",method = RequestMethod.POST )
 	public String getBasketPage() {
 		
 		return "/basket";
---
+++ spring_project/SinchonBeer3-shop&cart/src/main/java/com/bitcamp/sc/controller/LoginModalTestController.java
@@ -0,0 +1,9 @@
+package com.bitcamp.sc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginModalTestController {
+	
+}
